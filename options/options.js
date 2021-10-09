0278b91f7a1d6ed97519fd87c782d224788ef06e
Crowdbotics <team@crowdbotics.com>
2021-10-09 21:10:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "blackpinerail_31276",
+  url: "https://blackpinerail_31276.botics.co",
+  api: "https://blackpinerail_31276.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
